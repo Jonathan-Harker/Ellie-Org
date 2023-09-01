46cc5657cd789d65622311b3493e1a6f0d9b9f3a
Jonathan <Jjh201104@gmail.com>
2023-09-01 21:25:55 +0100

    feat: Turn app into a PWA

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const link = document.createElement('link');
+link.rel = 'manifest';
+link.href = '/manifest.json';
+document.head.appendChild(link);
